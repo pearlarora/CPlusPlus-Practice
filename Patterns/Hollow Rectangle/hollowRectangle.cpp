711386d78251aaedec2d704e2df3b355549de76f
Pearl Arora <ipearlarora@gmail.com>
2021-09-04 21:19:23 +0530

    i/o problem solved

--- Patterns/Hollow Rectangle/hollowRectangle.cpp
+++ Patterns/Hollow Rectangle/hollowRectangle.cpp
@@ -6,9 +6,15 @@ using std::cout;
 int main() {
   int rows, columns;
   cin >> rows >> columns;
+  
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < columns; j++) {
-      cout << "* ";
+      if (i==0 || i==(rows-1) || j==0 || j==(columns-1)) {
+        cout << "* ";
+      }
+      else {
+        cout << "  ";
+      }
     }
     cout << "\n";
   }
